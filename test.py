6ba8d4a2eca0b2f83d75ceec21a16002c7787e1a
josepablito16 <josepablocif16@gmail.com>
2020-09-05 10:23:13 -0600

    Se agrega script para hacer hacer request a la API

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import os
 from dotenv import load_dotenv
 import csv
 load_dotenv()
-#http://docs.tweepy.org/en/v3.5.0/api.html#API.search
-#https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/twitter-data-in-python/
+# http://docs.tweepy.org/en/v3.5.0/api.html#API.search
+# https://www.earthdatascience.org/courses/use-data-open-source-python/intro-to-apis/twitter-data-in-python/
 consumer_key = os.getenv("CONSUMER_KEY")
 consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
@@ -16,23 +16,22 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 # Setting your access token and secret
 auth.set_access_token(access_token, access_token_secret)
 # Creating the API object while passing in auth information
-api = tweepy.API(auth) 
+api = tweepy.API(auth)
 
 # The search term you want to find
-query = "giammattei + gobierno Guatemala"
+query = "giammattei until:2020-01-01"
 # Language code (follows ISO 639-1 standards)
 language = "es"
 
-date_since="2020-1-1"
+date_until = "2020-01-01"
+NOMBRECSV = 'enero'
 
 # Calling the user_timeline function with our parameters
 #results = api.search(api.search,q=query,lang=language,since=date_since)
 
-with open('files/test.csv', 'w', newline='',encoding="utf8") as file:
-	writer = csv.writer(file)
-	writer.writerow(["id", "date", "text"])
-	for page in tweepy.Cursor(api.search,q=query,lang=language,since=date_since,count=200).pages(4):
-		for status in page:
-			status2 = api.get_status(status._json['id'], tweet_mode = "extended")
-			writer.writerow([status._json['id'],status._json['created_at'],str(status2.full_text)])		
-			
+with open('files/'+NOMBRECSV+'.csv', 'w', newline='', encoding="utf8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["id", "date", "text"])
+    for page in tweepy.Cursor(api.search, q=query, lang=language, result_type="mixed", count=100, tweet_mode="extended").items(1000):
+        writer.writerow(
+            [page._json['id'], page._json['created_at'], page.full_text])
---
+++ test2.py
@@ -0,0 +1,66 @@
+import requests
+import base64
+import os
+from dotenv import load_dotenv
+load_dotenv()
+'''
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCES_SECRET")
+'''
+
+client_key = os.getenv("CONSUMER_KEY")
+client_secret = os.getenv("CONSUMER_SECRET")
+
+
+key_secret = '{}:{}'.format(client_key, client_secret).encode('ascii')
+b64_encoded_key = base64.b64encode(key_secret)
+b64_encoded_key = b64_encoded_key.decode('ascii')
+
+
+base_url = 'https://api.twitter.com/'
+auth_url = '{}oauth2/token'.format(base_url)
+
+auth_headers = {
+    'Authorization': 'Basic {}'.format(b64_encoded_key),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+}
+
+auth_data = {
+    'grant_type': 'client_credentials'
+}
+
+auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
+print(auth_resp.status_code)
+access_token = auth_resp.json()['access_token']
+search_headers = {
+    'Authorization': 'Bearer {}'.format(access_token)
+}
+
+search_params = {
+    'q': 'hola',
+    'result_type': 'mixed',
+    'lang': 'es',
+    'tweet_mode': 'extended',
+    'count': 2,
+    'until': '2020-09-01'
+}
+
+search_url = '{}1.1/search/tweets.json'.format(base_url)
+
+search_resp = requests.get(
+    search_url, headers=search_headers, params=search_params)
+
+tweet_data = search_resp.json()
+print()
+# print(tweet_data)
+
+
+for tweet in tweet_data['statuses']:
+    print(tweet['full_text'])
+    print(str(tweet['id']))
+    print(tweet['created_at'])
+    print('----------------------------------------------')
+    #print(tweet['retweeted_status'] + '\n')
